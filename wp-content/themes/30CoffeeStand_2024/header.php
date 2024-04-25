8292d31030463780ac37f400adbf687ca2c7f8f2
makino honoka <makino@onkuri-web.com>
2024-04-25 09:22:18 +0900

    メイン画像、favicon、OGP設定

--- wp-content/themes/30CoffeeStand_2024/header.php
+++ wp-content/themes/30CoffeeStand_2024/header.php
@@ -3,7 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/icon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri();?>/icon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/icon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <?php wp_head(); ?>
   </head>
   <body>
